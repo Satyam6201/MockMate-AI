ec90c1a03bd8c2ef0c4b12e9a20f1763a6161c0b
Satyam6201 <satyamkmishraa@gmail.com>
2026-09-23 11:13:37 +0530

    fix bugs

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -2,7 +2,11 @@ import mongoose from "mongoose";
 
 const db = async () => {
     try {
-        await mongoose.connect(process.env.MONGODB_URL);
+        await mongoose.connect(process.env.MONGODB_URL, {
+            maxPoolSize: 200,
+            minPoolSize: 20,
+            socketTimeoutMS: 45000
+        });
         console.log("Database is connected!");
     } catch (error) {
         console.log(`Error from the database ${error}`);
--- Backend/controllers/auth.controller.js
+++ Backend/controllers/auth.controller.js
@@ -16,7 +16,7 @@ export const googleAuth = async (req, res) => {
 
         let token = await genToken(user._id);
         res.cookie("token", token, {
-            http: true,
+            httpOnly: true,
             secure: false,
             sameSite: "strict",
             maxAge: 7*24*60*60*1000, // maxage is 7 days 
@@ -31,7 +31,7 @@ export const googleAuth = async (req, res) => {
 // Controller for the logout
 export const logout = async (req, res) => {
     try {
-        await res.clearCookie("token");
+        res.clearCookie("token");
         return res.status(200).json({message: "Logout Successfully"});
     } catch (error) {
         return res.status(500).json({message: `Logout error ${error}`});
